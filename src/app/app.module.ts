276a5c5d239d5a151d01e85ee90088954a8304e5
Javier Vidal <javier.vidal@arca-soft.com>
2020-01-02 03:42:31 -0600

    Se añade barra de navegacion
    Se añadió la barra de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
