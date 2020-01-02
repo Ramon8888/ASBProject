7841035a11cb72ecbdd868c7ac29a66b9c3282e1
Ramon Arellano Castro <ramo.arellano@infotec.mx>
2020-01-02 04:26:43 -0600

    Prueba de componente de registro
    Se añade prueba para componente de registro de usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component' ;
 import {NavegacionComponent } from './navegacion/navegacion.component' ;
+import {RegistroUsuariosComponent } from './registro-usuarios/registro-usuarios.component' ;
 
 
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent } ,
-  { path: 'navegacion' , component: NavegacionComponent } 
+  { path: 'navegacion' , component: NavegacionComponent },
+  { path: 'registro-usuarios' , component: RegistroUsuariosComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavegacionComponent } from './navegacion/navegacion.component';
+import { RegistroUsuariosComponent } from './registro-usuarios/registro-usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavegacionComponent
+    NavegacionComponent,
+    RegistroUsuariosComponent
   ],
   imports: [
     BrowserModule,
